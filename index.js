bd382175c80f2184465c95e57551f9f44f687601
User <user@Users-MacBook-Pro.local>
2020-01-12 17:57:36 +0200

    fourth

--- index.js
+++ index.js
@@ -10,3 +10,4 @@ document.getElementById("page").onmouseup = function(e) {
     document.getElementById("magnify").style.top = e.pageY-88 + "px";
     document.getElementById("magnify").style.left = e.pageX-88 + "px";
 }
+
