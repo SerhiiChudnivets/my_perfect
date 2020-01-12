0978e4ec5ee2ef8ecf6f9a8826de94804e591f13
User <user@Users-MacBook-Pro.local>
2020-01-12 17:52:41 +0200

    third

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ document.getElementById("page").onmousemove = function(e) {
     document.getElementById("magnify").style.top = e.pageY-88 + "px";
     document.getElementById("magnify").style.left = e.pageX-88 + "px";
 }
+document.getElementById("page").onmousedown = function(e) {
+    document.getElementById("magnify").style.top = e.pageY-138 + "px";
+    document.getElementById("magnify").style.left = e.pageX-138 + "px";
+}
+document.getElementById("page").onmouseup = function(e) {
+    document.getElementById("magnify").style.top = e.pageY-88 + "px";
+    document.getElementById("magnify").style.left = e.pageX-88 + "px";
+}
